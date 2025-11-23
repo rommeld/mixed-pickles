3f97fde510a95d9a5738f1c712e9961bf228b78f
rommeld <rommel.denny@gmail.com>
2025-11-23 19:04:35 +0100

    feat: added command to execute commit 5c8b35a138a28f5cddb7ec85868114634669a795 Author: rommeld <138243859+rommeld@users.noreply.github.com> Date:   Sun Nov 23 14:37:36 2025 +0100
        Add GitHub Actions workflow for Rust project
    
    commit 707e3b0c5f9303090ee1591494888922176d5111
    Author: rommeld <rommel.denny@gmail.com>
    Date:   Sun Nov 23 14:34:16 2025 +0100
    
        Docs: update README.md
    
    commit b49596879bb8734fcdad4e2a3f60aaf89495e291
    Author: rommeld <138243859+rommeld@users.noreply.github.com>
    Date:   Wed Nov 19 16:11:15 2025 +0100
    
        Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+use std::io;
+use std::path::Path;
+use std::process::Command;
+
+pub fn fetch_log() -> io::Result<String> {
+    // Check if .git directory exists before attempting to run git log
+    get_git_dir()?;
+
+    // Run `git log` to fetch all commits
+    let log = Command::new("git").arg("log").output()?;
+
+    if log.status.success() {
+        Ok(String::from_utf8_lossy(&log.stdout).to_string())
+    } else {
+        Err(io::Error::other(
+            // TODO: Use `stderr` for managing error messages
+            String::from_utf8_lossy(&log.stderr).to_string(),
+        ))
+    }
+}
+
+fn get_git_dir() -> io::Result<()> {
+    if Path::new(".git").exists() {
+        Ok(())
+    } else {
+        Err(io::Error::new(
+            io::ErrorKind::NotFound,
+            ".git directory not found",
+        ))
+    }
+}
+
+// TODO: Add tests
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, mixed pickles.")
+use mixed_pickles::fetch_log;
+use std::io;
+
+fn main() -> io::Result<()> {
+    match fetch_log() {
+        Ok(log) => println!("{}", log),
+        Err(e) => eprintln!("Failed to run git log: {}", e),
+    }
+    Ok(())
 }
