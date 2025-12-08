3df02c498a7772b5ee60be429c298a2d94e31cfa
rommeld <rommel.denny@gmail.com>
2025-12-08 14:02:02 +0100

    feat: add limit logs and transfer from Vec<u8> to String

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::{io, process::Command};
 
 fn main() -> Result<(), io::Error> {
-    let log_command = Command::new("git").arg("log").arg("--oneline").output()?;
-    println!("{:?}", log_command);
+    let log_command = Command::new("git").arg("log").args(["-n","5"]).output()?;
+    println!("{:?}", String::from_utf8_lossy(&log_command.stdout));
     Ok(())
 }
