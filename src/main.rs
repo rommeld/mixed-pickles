26ec3a6b943a0eb17d428cb2ae2da09acce5ed28
rommeld <rommel.denny@gmail.com>
2025-12-08 20:33:37 +0100

    feat: create Vec<&str> correctly and check for vector length

--- src/main.rs
+++ src/main.rs
@@ -12,22 +12,35 @@ struct Commit {
 fn main() -> Result<(), io::Error> {
     let log_command = Command::new("git")
         .arg("log")
-        .arg("--pretty=format:'%H|%an|%ae|%s'")
+        .arg("--pretty=format:%H|%an|%ae|%s")
         .args(["-n", "5"])
         .output()?;
-    
-    let log_output = String::from_utf8_lossy(&log_command.stdout).to_string(); 
-    
-    let mut log_opt = log_output.lines().next().expect("Failed to fetch first log.").split("|");
 
-    let commit = Commit {
-        hash: log_opt.next().expect("Failed to get hash.").to_string(),
-        author_name: log_opt.next().expect("Failed to get hash.").to_string(),
-        author_email: log_opt.next().expect("Failed to get hash.").to_string(),
-        subject: log_opt.next().expect("Failed to get hash.").to_string(),
+    let log_output = String::from_utf8_lossy(&log_command.stdout);
+
+    let log_opt = log_output
+        .lines()
+        .next()
+        .expect("Failed to fetch first log.")
+        .split("|");
+
+    let mut log_vector = Vec::new();
+
+    for i in log_opt {
+        log_vector.push(i)
+    }
+
+    if log_vector.len() == 4 {
+        let commit = Commit {
+            hash: log_vector[0].to_string(),
+            author_name: log_vector[1].to_string(),
+            author_email: log_vector[2].to_string(),
+            subject: log_vector[3].to_string(),
+        };
+        println!("{:?}", commit);
+    } else {
+        println!("Log couldn't be loaded.")
     };
 
-    println!("{:#?}", commit);
-    
     Ok(())
 }
