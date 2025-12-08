5665b8d3a669eff83185a758bd44f1958ef153f4
rommeld <rommel.denny@gmail.com>
2025-12-08 18:46:00 +0100

    feat: dirty implementation of creating 'Commit' struct

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,33 @@
 use std::{io, process::Command};
 
+#[derive(Debug)]
+struct Commit {
+    hash: String,
+    author_name: String,
+    // TODO: validate email with '@'
+    author_email: String,
+    subject: String,
+}
+
 fn main() -> Result<(), io::Error> {
-    let log_command = Command::new("git").arg("log").args(["-n","5"]).output()?;
-    println!("{:?}", String::from_utf8_lossy(&log_command.stdout));
+    let log_command = Command::new("git")
+        .arg("log")
+        .arg("--pretty=format:'%H|%an|%ae|%s'")
+        .args(["-n", "5"])
+        .output()?;
+    
+    let log_output = String::from_utf8_lossy(&log_command.stdout).to_string(); 
+    
+    let mut log_opt = log_output.lines().next().expect("Failed to fetch first log.").split("|");
+
+    let commit = Commit {
+        hash: log_opt.next().expect("Failed to get hash.").to_string(),
+        author_name: log_opt.next().expect("Failed to get hash.").to_string(),
+        author_email: log_opt.next().expect("Failed to get hash.").to_string(),
+        subject: log_opt.next().expect("Failed to get hash.").to_string(),
+    };
+
+    println!("{:#?}", commit);
+    
     Ok(())
 }
